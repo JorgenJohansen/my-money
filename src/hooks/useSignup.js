747df56b0c43f63bc2337205ed21df0096f0fe57
jørgen buholm johansen <jorgen_buholm_johansen@hotmail.com>
2024-02-01 19:18:50 +0100

    initial commit

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react"
+import { auth } from "../firebase/config"
+import { useAuthContext } from "./useAuthContext";
+
+export const useSignup = () => {
+    const [isCancelled, setIsCancelled] = useState(false)
+    const [ error, setError ] = useState(null);
+    const [ isPending, setIsPending ] = useState(false);
+    const { dispatch } = useAuthContext();
+
+    const signup = async (email, password, displayName) => {
+        setError(null);
+        setIsPending(true);
+
+        try{
+            const res = await auth.createUserWithEmailAndPassword(email, password);
+            console.log(res.user);
+
+            if(!res){
+                throw new Error("Could not complete signup")
+            }
+
+            await res.user.updateProfile({ displayName });
+
+            dispatch({type: 'LOGIN', payload: res.user});
+
+            // update state
+            if(!isCancelled){
+                setIsPending(false);
+                setError(null);
+            }
+        }catch(err){
+            if(!isCancelled){
+                console.log(err)
+                setError(err.message)
+                setIsPending(false)
+            }
+        }
+    }
+
+    useEffect(() => {
+        return () => setIsCancelled(true)
+    }, [])
+
+    return { error, isPending, signup}
+}
